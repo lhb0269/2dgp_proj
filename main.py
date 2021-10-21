54d84f06a3c4f1511533ea577395575ac5381739
lhb0269 <lhb0269@gmail.com>
2021-10-21 12:36:14 +0900

    기타 수정

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import random
 def handle_events():
    global running
    global dir
@@ -54,24 +55,23 @@ class HERO:
         self.endy=self.y+200
         self.lookright = True #캐릭터 보고 있는 방향 체크
         self.Falling = False
-    def update(self,mon,block):
+    def update(self,mon,blocks):
         global jumping
         self.frame=(self.frame+1)%8
-        if (mon.x == self.x and mon.y == self.y) or (mon.x+60 == self.x and mon.y == self.y):#죽음 판정
+        if (mon.x == self.x and mon.y+10 >= self.y) or (mon.x+80 == self.x and mon.y+10 >= self.y):#죽음 판정
             self.x = 1000
-        if self.x>=mon.x and self.x<=mon.x+77 and self.y <= mon.y + 60 and self.Falling == True:# 몬스터 피격
+        if self.x>=mon.x-20 and self.x<=mon.x+80 and self.y <= mon.y + 60 and self.Falling == True:# 몬스터 사망
             mon.x=1000
-        if self.x>=block.x-40 and self.x<=block.x+60 and self.y >= block.y-50  and jumping == True: #벽 위로 박기
-            jumping = False
-            self.Falling = True
-            block.life -= 1
-        # if self.x>=block.x-40 and self.x<=block.x+60 and self.y == block.y+53  and jumping == False:#벽 위에 서기
-        #     self.Falling = False
-        #     self.miny = self.y+3
-        #     self.y+=3
-        #     self.endy = self.y + 200
-        if (self.y>self.miny):
-            self.y-=0.1
+        for block in blocks:
+         if self.x>=block.x-40 and self.x<=block.x+40 and self.y >= block.y-50  and jumping == True: #벽 위로 박기
+                jumping = False
+                self.Falling = True
+                block.life -= 1
+        if self.x>=block.x and self.x<=block.x+60 and self.y <= block.y+50  and self.y >= block.y and jumping == False:#벽 위에 서기
+            self.Falling = False
+            self.miny = self.y-1
+            self.y+=3
+            self.endy = self.y + 200
     def jump(self):
         global jumping
         if jumping == True and self.Falling == False:
@@ -95,6 +95,8 @@ class HERO:
         if dir == 1:
             self.image.clip_composite_draw(self.frame * 50, 0 ,50, 50, 2*3.14,'h',self.x, self.y,50,50)
             self.lookright = False
+        if jumping == True or self.Falling == True:
+            self.frame = 1
         if self.lookright == True and dir == 0:
             self.image2.draw(self.x,self.y)
         if self.lookright == False and dir == 0:
@@ -137,8 +139,8 @@ class WOODS:
     def draw(self):
         self.image.clip_draw(22, 142, 290, 70, self.x,self.y)
 class BLOCK:
-    def __init__(self):
-        self.x = 500
+    def __init__(self,xpos):
+        self.x = xpos
         self.y = 100
         self.image = load_image('sprite.png')
         self.life = 3
@@ -152,7 +154,8 @@ class BLOCK:
         if self.life % self.i != 0:
             self.i -= 1
         if self.life ==0:
-            self.x = 1000
+            self.x +=random.randint(1,5)*60+480
+            self.life=3
     def draw(self):
         for i in range(2, 5):
             self.y += i
@@ -170,16 +173,17 @@ cloud = CLOUDS()
 mountain = Mountain()
 woods = WOODS()
 cs = castle()
-block = BLOCK()
+blocks = [BLOCK(400),BLOCK(460),BLOCK(520)]
 
 def Update_Obj():
     mon.update()
-    hero.update(mon,block)
+    hero.update(mon,blocks)
     cloud.update()
     mountain.update()
     woods.update()
     cs.update()
-    block.update()
+    for block in  blocks:
+        block.update()
 
 def Draw_obj():
     background.draw(200, 200)
@@ -190,7 +194,8 @@ def Draw_obj():
     cs.draw()
     mon.draw()
     hero.draw()
-    block.draw()
+    for block in blocks:
+        block.draw()
 
 while running:
     #update obj
